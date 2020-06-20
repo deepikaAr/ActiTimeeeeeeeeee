eaa1d934ddbc66c7d571abbd7da70a593aab25a0
Deepika <deepikacoo@gmail.com>
2020-06-20 12:31:05 +0530

    day1

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,13 @@
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("deepikapriank");
+		System.out.println("he");
+
+	}
+
+}
